d31e106fb2a784a033cb4f990ac15e10438dcc35
ZiyadZK <kakangtea74@gmail.com>
2025-12-11 15:28:48 +0700

    (DEBUGGING PURPOSE)

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home () {
     return (
         <div className="w-full min-h-screen flex flex-col items-center justify-center bg-white text-zinc-700">
             <h1 className="text-6xl tracking-tighter">
-                Base Code
+                Base Code FE
             </h1> 
             <p>
                 Edit Halaman di resources/js/Pages/Home.jsx
